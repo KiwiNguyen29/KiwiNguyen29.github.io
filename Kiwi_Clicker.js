a5ffaf38c96fe8d18719f3fd5abde8f80977ca67
KiwiNguyen29 <quockien.nguyen29@student.tamdistrict.org>
2025-12-04 16:59:32 +0000

    kiwiclick v3

--- Kiwi_Clicker.js
+++ Kiwi_Clicker.js
@@ -1,23 +1,27 @@
 let countEl = document.getElementById("count-el");
-let upgrade = document.getElementById("upgrade-btn")
+let upgrade = document.getElementById("upgrade-btn");
 console.log(countEl);
 
 let count = 0;
-let multiplier = 1;
-let clickcount = 1 
-let clickmultiplier = 1
+let clickcount = 1;
+let clickmultiplier = 1;
 
 function increment() {
-    count = count + clickcount * clickmultiplier ;
-    countEl.innerText = count;
+  count = count + clickcount * clickmultiplier;
+  countEl.innerText = count;
 }
+kiwibtn.addEventListener("click", increment1)
 
 function increment2() {
- clickcount = 2
- count = count - 10
+  clickcount = 2;
+  count = count - 10;
+  countEl.innerText = count;
 }
+kiwibtn.addEventListener("click", increment2)
 
 function increment3() {
- clickmultiplier = 2
- count = count - 50
+  clickmultiplier = 2;
+  count = count - 50;
+  countEl.innerText = count;
 }
+kiwibtn.addEventListener("click", increment3)
